b7677b7e9c4c489dd887a751ed90c75173f4e543
Juan Carlos <juancarlospaco@gmail.com>
2024-06-13 14:27:15 -0300

    Delete plugins/dummy.py

--- plugins/dummy.py
+++
@@ -1,4 +0,0 @@
-def main(config):
-    print(f"Example minimal plugin '{__name__}' running with config '{config}'.")
-    # Perform plugin-specific operations using the config data...
-    return True
