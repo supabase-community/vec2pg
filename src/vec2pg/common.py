50cee3b7786115e1874485bfaca7cf82d0141d6f
Juan Carlos <juancarlospaco@gmail.com>
2024-11-02 23:14:03 -0300

    feat: Check if url is an HTTP URL

--- src/vec2pg/common.py
+++ src/vec2pg/common.py
@@ -1,1 +1,8 @@
+from urllib.parse import urlparse
+
+
 POSTGRES_CONNECTION_STRING = "POSTGRES_CONNECTION_STRING"
+
+
+def is_http_url(url: str) -> bool:
+    return urlparse(url).scheme in {"https", "http"}
